ba19db46768da13760f25a8b528a36f036822cc4
kuba_sway <jakubekc@gmail.com>
2025-01-17 23:47:52 +0100

    Zaktualizowano pliki
    zaktualizowano pliki Zadanie7.cpp i Zadanie8.cpp

---
+++ Zadanie7.cpp
@@ -0,0 +1,17 @@
+﻿// Zadanie7.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Witaj! To jest program, ktory obliczy pole okregu na podstawie wartosci promienia r, ktory podasz w nastepnym kroku\n";
+    cout << "Podaj promien r:\n";
+    double r;
+    cin >> r;
+    double pole;
+    pole = 3.14 * r * r;
+    cout << "Pole okregu o promieniu " << r << " cm Wynosi: " << pole << " cm kwadratowych";
+    return 0;
+}
---
+++ Zadanie8.cpp
@@ -0,0 +1,17 @@
+﻿// Zadanie8.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    string imie;
+    int rok;
+    cout << "Podaj Imie\n";
+    cin >> imie;
+    cout << "Podaj rok rozpoczecia studiow\n";
+    cin >> rok;
+    cout << "Student " << imie << " rozpoczal nauke w roku " << rok;
+
+}
+
